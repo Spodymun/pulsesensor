525606ba38dc771870e528db2b3382a97a32d353
Spodymun <n.schrader@ostfalia.de>
2024-03-22 16:31:35 +0100

    Fast fertig, aber Arduino sagen Nein :(

--- Arduino/simplecommunication/simplecommunication.ino
+++ Arduino/simplecommunication/simplecommunication.ino
@@ -2,7 +2,7 @@
 #include <WiFiUdp.h>
 #include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
 
-const char* ssid = "Schnelly";
+const char* ssid = "schnelly";
 const char* password = "essenkochen23";
 
 WiFiUDP Udp;
@@ -19,9 +19,11 @@ void setup() {
   initSocket();
 }
 
+//Main Schleife, misst und sendet Daten
 void loop() {
   if (pulseSensor.sawStartOfBeat()) {
     int myBPM = pulseSensor.getBeatsPerMinute();
+    Serial.println(myBPM);
     sendToPC(myBPM);
   }
   delay(20); 
@@ -29,11 +31,13 @@ void loop() {
 
 // -------------
 
+//Soll Verbindung zum Wlan herstellen
 void connectToWifi() {
   WiFi.begin(ssid, password);
   waitUntilConnectionIsAvailable();
 }
 
+//Überprüft ob die Verbindung aktuell gegeben ist
 void waitUntilConnectionIsAvailable() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -47,14 +51,15 @@ void setupPulseSensor() {
   pulseSensor.begin();
 }
 
+//UDP Begin muss aufgerufen werden, wird jedoch nicht benötigt
 void initSocket() {
   Udp.begin(42069);
 }
 
 void sendToPC(int bpm) {
   char bpm_as_char_array[4];
-  itoa(bpm, bpm_as_char_array, 10); //Convert int to char[]
-  Udp.beginPacket("192.168.137.1", 12000);  // Hard coded ip and port. We know IP-Address because pc is alwas host
+  itoa(bpm, bpm_as_char_array, 10); //Konvertiert int to char[]
+  Udp.beginPacket("192.168.137.1", 12000);  // Hard coded ip und port. Wir wissen die IP-Adresse, weil der Pc immer der Host ist
   Udp.write(bpm_as_char_array);
   Udp.endPacket();
 }
